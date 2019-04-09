25a3769f02ae4cb435c0eabde744016264d1d66c
shijieheping <ldqhyfzpz@qq.com>
2019-04-09 10:29:46 +0800

    learning git, how to add and commit

---
+++ 10.0-cudnn7-devel-ubuntu16.04/mofed451010/nccl237/ompi400/nccl-tests-osu/tensorflow112-noverbs/benchmarks/horovod/build.sh
@@ -0,0 +1,1 @@
+docker build . -t aaii/apollo6500:10.0-cudnn7-devel-ubuntu16.04-mofed451010-nccl237-ompi400-nccltest-osu-tensorflow112-noverbs-benchmarks-horovod
---
+++ 9.2-cudnn7-devel-ubuntu16.04/mofed451010/nccl237/ompi400/nccl-tests-osu/tensorflow112-noverbs/benchmarks/horovod/build.sh
@@ -0,0 +1,1 @@
+docker build . -t aaii/apollo6500:9.2-cudnn7-devel-ubuntu16.04-mofed451010-nccl237-ompi400-nccltest-osu-tensorflow112-noverbs-benchmarks-horovod
